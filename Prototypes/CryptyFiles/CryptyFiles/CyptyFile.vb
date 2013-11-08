cb9229062c0fa7fde542871d377d54cd321e5f25
Evert Geldenhuys <evert.geldenhuys@hotmail.com>
2013-11-08 16:26:16 +1100

    Add CryptyFiles Prototype
    File Management prototype

---
+++ Prototypes/CryptyFiles/CryptyFiles/CyptyFile.vb
@@ -0,0 +1,64 @@
+﻿Public Class CyptyFile
+
+    ' Name
+    ' Path
+    ' Hash
+    ' Size
+    ' Status
+
+    Private _name As String
+    Private _path As String
+    Private _hash As String
+    Private _size As Integer
+    Private _status As String
+
+    Public Property Name
+        Set(ByVal value)
+            _name = value
+
+        End Set
+
+        Get
+            Return _name
+
+        End Get
+    End Property
+
+    Public Property Path
+        Set(ByVal value)
+            _path = value
+
+        End Set
+
+        Get
+            Return _path
+        End Get
+    End Property
+
+    Public ReadOnly Property Hash
+        Get
+            Return _hash
+
+        End Get
+    End Property
+
+    Public ReadOnly Property Size
+
+        Get
+            Return _size
+
+        End Get
+    End Property
+
+    Public ReadOnly Property Status
+        Get
+            Return _status
+
+        End Get
+    End Property
+
+    ' TODO: Throw exception and observer results
+    Private Sub CheckInput(ByVal text As String)
+
+    End Sub
+End Class
---
+++ Prototypes/CryptyFiles/README.md
@@ -0,0 +1,18 @@
+##CryptyFiles Prototype
+
+Prototype of File Management
+
+* File management
+	* Add file
+	* Remove file
+	* Edit file
+	* Delete File
+	* Save Files
+	* Load Files
+	
+* CryptyFile object
+	* File name
+	* Path
+	* Status
+	* Hash
+	* Size
