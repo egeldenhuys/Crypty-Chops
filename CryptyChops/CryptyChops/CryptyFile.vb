5051c8330c2c51c5c3091d4ba74256c74228c937
Evert Geldenhuys <evert.geldenhuys@hotmail.com>
2013-12-24 11:37:01 +1100

    Create framework for CryptyFile

--- CryptyChops/CryptyChops/CryptyFile.vb
+++ CryptyChops/CryptyChops/CryptyFile.vb
@@ -1,4 +1,54 @@
-﻿Public Class CryptyFile
+﻿Imports System.IO
 
-    ' Empty class for references
+Public Class CryptyFile
+
+    Private _name As String ' The name of the object, not the actual file name.
+    Private _fileInfo As FileInfo
+    Private _key As String
+
+
+    Public Sub Encrypt()
+
+    End Sub
+
+    Public Sub Decrypt()
+
+    End Sub
+
+    Public Sub Compress()
+
+    End Sub
+
+    Public Sub Extract()
+
+    End Sub
+
+#Region "Properties"
+
+    Public Property Key() As String
+        Get
+            Return _key
+        End Get
+        Set(ByVal value As String)
+            _key = value
+        End Set
+    End Property
+
+    Public Property Name() As String
+        Get
+            Return _name
+        End Get
+        Set(ByVal value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public ReadOnly Property FileInfo() As FileInfo
+        Get
+            Return _fileInfo
+        End Get
+    End Property
+
+#End Region
+
 End Class
