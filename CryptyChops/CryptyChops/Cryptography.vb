7f0be6f4d2d83657d8ba5301536a9bdaadfe0958
Evert Geldenhuys <evert.geldenhuys@hotmail.com>
2014-01-03 23:35:17 +1100

    Create Cryptography Class structure

---
+++ CryptyChops/CryptyChops/Cryptography.vb
@@ -0,0 +1,16 @@
+﻿Public Class Cryptography
+    ' This class will store the functions for encrypting and decrypting bytes
+
+    Public Class Reverse
+
+    End Class
+
+    Public Class AES
+
+    End Class
+
+    Public Class CryptyEncrypt
+
+    End Class
+
+End Class
