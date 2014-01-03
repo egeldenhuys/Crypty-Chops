d906a0ffdcf901cda5c202782971b7dd822c758a
Evert Geldenhuys <evert.geldenhuys@hotmail.com>
2014-01-04 08:01:17 +1100

    Add public functions

--- CryptyChops/CryptyChops/Cryptography.vb
+++ CryptyChops/CryptyChops/Cryptography.vb
@@ -1,15 +1,33 @@
 ﻿Public Class Cryptography
     ' This class will store the functions for encrypting and decrypting bytes
 
+    Const BUFFER_SIZE As Integer = 1024
+
     Public Class Reverse
+
+        Public Shared Sub ApplyAlgortihm(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
 
     End Class
 
     Public Class AES
 
+        Public Shared Sub Encrypt(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
+        Public Shared Sub Descrypt(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
     End Class
 
     Public Class CryptyEncrypt
+
+        Public Shared Sub ApplyAlgortihm(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
 
     End Class
 
