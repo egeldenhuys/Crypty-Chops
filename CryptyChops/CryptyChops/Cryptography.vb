b6989c54fa9e15bdeddd03f4136b568e72f94c31
Evert Geldenhuys <evert.geldenhuys@hotmail.com>
2014-01-04 08:07:43 +1100

    Copy Cryptography class from master

---
+++ CryptyChops/CryptyChops/Cryptography.vb
@@ -0,0 +1,34 @@
+﻿Public Class Cryptography
+    ' This class will store the functions for encrypting and decrypting bytes
+
+    Const BUFFER_SIZE As Integer = 1024
+
+    Public Class Reverse
+
+        Public Shared Sub ApplyAlgortihm(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
+    End Class
+
+    Public Class AES
+
+        Public Shared Sub Encrypt(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
+        Public Shared Sub Descrypt(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
+    End Class
+
+    Public Class CryptyEncrypt
+
+        Public Shared Sub ApplyAlgortihm(sourcePath As String, destPath As String, key() As Byte)
+
+        End Sub
+
+    End Class
+
+End Class
